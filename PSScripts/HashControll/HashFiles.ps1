1332082b43ca5b6c1a6661068e9652b1363bd193
Tobias Hattinger <tobias.hattinger@gmx.at>
2019-09-23 13:43:27 +0200

    Added FileHashing-Script

---
+++ PSScripts/HashControll/HashFiles.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module PSSQLite
+Get-Command -Module PSSQLite
+
+$targetDirectory = "D:\ClientShareClones"
+
+
+$database = "C:\Users\Administrator.HTSTORAGE\Desktop\hashInfo.db"
+
+Write-Host -ForegroundColor Yellow "Starting..."
+
+foreach($file in Get-ChildItem $targetDirectory -Recurse -Force -File)
+{
+    $hash = Get-FileHash $file.FullName -Algorithm SHA256
+
+    Write-Host "$($hash.Hash) - $($file.FullName)"
+
+    $query = "INSERT INTO files (Filename, Hash) VALUES (@Filename, @Hash)"
+    Invoke-SqliteQuery -Database $database -Query $query -SqlParameters @{
+        Filename = $file.FullName
+        Hash = $hash.Hash
+    }
+}
+
+pause
---
+++ PSScripts/HashControll/HashFiles2.ps1
@@ -0,0 +1,39 @@
+﻿Import-Module PSSQLite
+Get-Command -Module PSSQLite
+
+$targetDirectory = "D:\ClientShareClones"
+
+
+$database = "C:\Users\Administrator.HTSTORAGE\Desktop\hashInfo.db"
+
+$dataTable = New-Object System.Data.DataTable 'hashfiles'
+$newCol = New-Object System.Data.DataColumn Filename,([string]); $dataTable.Columns.Add($newCol)
+$newCol = New-Object System.Data.DataColumn Hash,([string]); $dataTable.Columns.Add($newCol)
+
+Write-Host -ForegroundColor Yellow "Starting..."
+
+$i = 0
+foreach($file in Get-ChildItem $targetDirectory -Recurse -Force -File)
+{   
+    if($i -ge 10000)
+    {
+        $i = 0
+        Write-Host -ForegroundColor Yellow "Pasting to DB..."
+        Invoke-SQLiteBulkCopy -DataTable $dataTable -DataSource $database -Table files -NotifyAfter 1000 -Verbose -Confirm:$false
+        $dataTable.Clear()
+    }
+    $i++
+
+    Write-Host -ForegroundColor Yellow "Cycle $i..."
+
+    $hash = Get-FileHash $file.FullName -Algorithm SHA256
+
+
+    $row = ($file.FullName, $hash.Hash)
+    $dataTable.Rows.Add($row)
+    
+
+}
+
+Write-Host -ForegroundColor Yellow "Pasting to DB..."
+Invoke-SQLiteBulkCopy -DataTable $dataTable -DataSource $database -Table files -NotifyAfter 10 -Verbose -Confirm:$false
